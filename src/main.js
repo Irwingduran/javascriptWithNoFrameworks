6e29ee14ea8e09e4d88ddf5915bf4b4b4976caf1
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-02-19 23:59:30 -0600

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let shop = document.getElementById("shop");
+
+let generateShop = () => {
+    return (shop.innerHTML =
+        <div class="item">
+            <img width="220" src="images/img-1.jpg" alt="">
+            <div class="details">
+                <h3>Casual Shirt</h3>
+                <p>Lorem ipsum dolor...</p>
+                <div class="price-quantity">
+                    <h2>$ 45</h2>
+                    <div class="buttons">
+                        <i class=""
+                    </div>
+                </div>
+            </div>
+
+        </div> 
+        )
+} 
